1c95aa5d2418426a8bab1355e9f5257cc3806fc6
B34RDY <dabeardmonsta@gmail.com>
2015-03-08 15:53:47 +0000

    Added heroku db info

--- connection.js
+++ connection.js
@@ -1,10 +1,10 @@
 var mysql = require('mysql');
 var connection = mysql.createConnection(
     {
-        host     : 'localhost',
-        user     : 'root',
-        password : '5t&g3c0&ch8u5',
-        database : 'annabel'
+        host     : process.env.DB_HOST || 'localhost',
+        user     : process.env.DB_USER || 'root',
+        password : process.env.DB_PASSWORD || '5t&g3c0&ch8u5',
+        database : process.env.DB_DATABASE || 'annabel'
     }
 );
 
