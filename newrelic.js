d49737123fcca9bf862ac8eea0bfeb2d58a7ddaf
B34RDY <dabeardmonsta@gmail.com>
2015-04-01 15:55:29 +0100

    Added New Relic information

---
+++ newrelic.js
@@ -0,0 +1,24 @@
+/**
+ * New Relic agent configuration.
+ *
+ * See lib/config.defaults.js in the agent distribution for a more complete
+ * description of configuration variables and their potential values.
+ */
+exports.config = {
+  /**
+   * Array of application names.
+   */
+  app_name : ['codewonk'],
+  /**
+   * Your New Relic license key.
+   */
+  license_key : '3018e97d859d5c93e1edb1a9856290d600683147',
+  logging : {
+    /**
+     * Level at which to log. 'trace' is most useful to New Relic when diagnosing
+     * issues with the agent, 'info' and higher will impose the least overhead on
+     * production applications.
+     */
+    level : 'info'
+  }
+};
